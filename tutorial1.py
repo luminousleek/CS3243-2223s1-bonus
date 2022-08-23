b6bf266aa87280911d131249ff703ee7b6178e63
Isaac Lee <68138671+luminousleek@users.noreply.github.com>
2022-08-23 18:46:30 +0800

    solve tutorial 1

--- tutorial1.py
+++ tutorial1.py
@@ -35,12 +35,23 @@ def dfs_graph(graph, goal_test, start='S'):
     return None
 
 def bfs_tree(graph, goal_test, start='S'):
-    # TODO Implement this
-    pass
+    frontier = [start]
+    while frontier:
+      node = frontier.pop(0) # remove first item from list
+      if goal_test(node):
+        return node
+      frontier.extend(sorted(graph[node]))
+    return None
 
 def bfs_graph(graph, goal_test, start='S'):
-    # TODO Implement this
-    pass
+    explored, frontier = set(), [start]
+    while frontier:
+        node = frontier.pop(0) # remove first item from list
+        if goal_test(node):
+            return node
+        explored.add(node)
+        frontier.extend(sorted(graph[node] - explored - set(frontier)))
+    return None
 
 def print_all(fns):
     for fn in fns:
@@ -54,19 +65,53 @@ print_all([dfs_tree, dfs_graph, bfs_tree, bfs_graph])
 
 # Now implement with early goal test to see the difference
 def dfs_early_tree(graph, goal_test, start='S'):
-    # TODO Implement this
-    pass
+    if goal_test(start):
+        return start
+    frontier = [start]
+    while frontier:
+        node = frontier.pop()
+        for nde in sorted(graph[node]):
+            if goal_test(nde):
+                return nde
+            frontier.append(nde)
+    return None
 
 def dfs_early_graph(graph, goal_test, start='S'):
-    # TODO Implement this
-    pass
+    if goal_test(start):
+      return start
+    explored, frontier = set(), [start]
+    while frontier:
+        node = frontier.pop()
+        for nde in sorted(graph[node] - explored - set(frontier)):
+          if goal_test(nde):
+            return nde
+          frontier.append(nde)
+        explored.add(node)
+    return None
 
 def bfs_early_tree(graph, goal_test, start='S'):
-    # TODO Implement this
-    pass
+    if goal_test(start):
+      return start
+    frontier = [start]
+    while frontier:
+        node = frontier.pop(0)
+        for nde in sorted(graph[node]):
+          if goal_test(nde):
+            return nde
+          frontier.append(nde)
+    return None
 
 def bfs_early_graph(graph, goal_test, start='S'):
-    # TODO Implement this
-    pass
+    if goal_test(start):
+      return start
+    explored, frontier = set(), [start]
+    while frontier:
+        node = frontier.pop(0)
+        for nde in sorted(graph[node] - explored - set(frontier)):
+          if goal_test(nde):
+            return nde
+          frontier.append(nde)
+        explored.add(node)
+    return None
 
 print_all([dfs_early_tree, dfs_early_graph, bfs_early_tree, bfs_early_graph])
